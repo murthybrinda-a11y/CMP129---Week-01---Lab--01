a1ddf2e261161f5e96512f52e9acbd1ea5c7d070
murthy.brinda <murthy.brinda@student.ccm.edu>
2026-09-03 23:25:17 -0400

    complete week 1 Lab 1

--- BoxOffice.java
+++ BoxOffice.java
@@ -1,4 +1,36 @@
-public class BoxOffice {
+import java.util.Scanner ;
+public class BoxOffice 
+
+{
+    public static void main(String args [])
+    {
+        Scanner keyboard = new Scanner (System.in);
+        String MovieName ;
+        int AdultTickets ;
+        int ChildTickets ;
+        double GrossProfit ;
+        double Netprofit ;
+        double TotalAmount ;
+        System.out.println("Enter the neme of the movie");
+        MovieName = keyboard.nextLine();
+        System.out.print("Enter the number of adult tickets sold ");
+        AdultTickets = keyboard.nextInt();
+        System.out.print("Enter the number of child tickets sold ");
+        ChildTickets = keyboard.nextInt();
+        AdultTickets = AdultTickets*10;
+        ChildTickets = ChildTickets *6;
+        GrossProfit = AdultTickets + ChildTickets ;
+        Netprofit = GrossProfit*0.20;
+        TotalAmount = GrossProfit - Netprofit;
+        System.out.println("Movie Name: "+MovieName);
+        System.out.println("Adult Tickets Sold: "+AdultTickets);
+        System.out.println("Child Tickets Sold: "+ChildTickets);
+        System.out.println("Gross Box Office Profit: $"+ GrossProfit);
+        System.out.println("Net Box Proft: $"+ Netprofit);
+        System.out.println("Amount Paid to Distributor: $"+ TotalAmount);
+
+    }
+
     
     
 }
